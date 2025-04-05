5678c6951e58a3783888c25ae6ca92365a27827a
Venugopal <venugopal2u@gmail.com>
2025-04-05 17:51:12 +0530

    test

--- provider.tf
+++ provider.tf
@@ -1,5 +1,6 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = var
+  access_key = github_actions_secret("AWS_ACCESS_KEY_ID")
+  secret_key = github_actions_secret("WS_SECRET_ACCESS_KEY")
 
 }
