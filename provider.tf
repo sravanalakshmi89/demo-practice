76c95a91650a6ff74b4b31a25bc27f548b85adca
Venugopal <venugopal2u@gmail.com>
2025-04-05 17:53:57 +0530

    Remove AWS access key and secret key from provider configuration

--- provider.tf
+++ provider.tf
@@ -1,6 +1,5 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = github_actions_secret("AWS_ACCESS_KEY_ID")
-  secret_key = github_actions_secret("WS_SECRET_ACCESS_KEY")
+  region = "us-east-1"
+
 
 }
