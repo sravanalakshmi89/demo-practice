3813ba79f108f6bee95d62701e984b1f46619d55
Venugopal <venugopal2u@gmail.com>
2025-04-06 16:11:10 +0530

    Add SSM agent installation and IAM role for EC2 instances

--- main.tf
+++ main.tf
@@ -4,8 +4,19 @@ resource "aws_instance" "example" {
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.sub1.id
   vpc_security_group_ids = [aws_security_group.sg.id]
-
+  iam_instance_profile   = aws_iam_instance_profile.ssm_instance_profile.name
+  user_data              = <<-EOF
+              #!/bin/bash
+              yum install -y amazon-ssm-agent
+              systemctl enable amazon-ssm-agent
+              systemctl start amazon-ssm-agent
+              EOF
   tags = {
     Name = "jenkins"
   }
+}
+
+resource "aws_iam_instance_profile" "ssm_instance_profile" {
+  name = "ec2-ssm-instance-profile"
+  role = aws_iam_role.ssm_role.name
 }
